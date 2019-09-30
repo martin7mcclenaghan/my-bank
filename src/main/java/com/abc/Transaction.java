6867fc50910fa5781e547cc75590938a64d010b6
martin7mcclenaghan <48988919+martin7mcclenaghan@users.noreply.github.com>
2019-09-30 02:25:29 +0100

    Package-Private and added TransactionType enum.

--- src/main/java/com/abc/Transaction.java
+++ src/main/java/com/abc/Transaction.java
@@ -1,16 +1,42 @@
 package com.abc;
 
-import java.util.Calendar;
 import java.util.Date;
 
-public class Transaction {
-    public final double amount;
+import static java.lang.Math.abs;
 
+class Transaction {
+
+    enum TransactionType {WITHDRAWAL, DEPOSIT};
+
+    private TransactionType transactionType;
+    private final double amount;
     private Date transactionDate;
 
-    public Transaction(double amount) {
+    Transaction(double amount) {
         this.amount = amount;
+
+        if(amount > 0){
+            this.transactionType = TransactionType.DEPOSIT;
+        } else {
+            this.transactionType = TransactionType.WITHDRAWAL;
+        }
+
         this.transactionDate = DateProvider.getInstance().now();
     }
 
+    String getTransactionType() {
+        return this.transactionType.name();
+    }
+
+    String getAmountString() {
+        return String.format("$%,.2f", abs(this.amount));
+    }
+
+    double getAmount (){
+        return this.amount;
+    }
+
+    String getTransactionDate() {
+        return this.transactionDate.toString();
+    }
 }
