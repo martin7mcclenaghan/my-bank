6a3ddbbed5d824e31dd6e4284da3a7f74de9f992
martin7mcclenaghan <48988919+martin7mcclenaghan@users.noreply.github.com>
2019-10-01 13:18:52 +0100

    Bank class made package private, organised and String concatonation changed to use StringBuffer.

--- src/main/java/com/abc/Main.java
+++ src/main/java/com/abc/Main.java
@@ -7,15 +7,20 @@ public class Main {
         Bank bank = new Bank();
         Customer martin = new Customer("Martin");
         bank.addCustomer(martin);
+        martin.openAccount(AccountType.CHECKING);
+        martin.getAccount(1).deposit(500);
         martin.openAccount(AccountType.SAVINGS);
+        martin.getAccount(2).deposit(100);
         System.out.println(martin.getNumberOfAccounts());
         System.out.println(martin.getStatement());
 
         Customer adam = new Customer("Adam");
         bank.addCustomer(adam);
         adam.openAccount(AccountType.CHECKING);
-        System.out.println(adam.getNumberOfAccounts());
+        adam.getAccount(3).deposit(500);
         System.out.println(adam.getStatement());
+
+        System.out.println(bank.customerSummary());
 
 
     }
